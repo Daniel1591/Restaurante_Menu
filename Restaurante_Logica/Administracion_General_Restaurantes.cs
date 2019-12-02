c44ca5e88f66f068dd38ad765212a08011c73412
Fauricio Calderon Quintana <a-fauriciocq@hotmail.com>
2019-12-02 15:24:40 -0600

    Creación de lógica para apartado administrativo general de restaurantes y estandarización de versión del Entity Framework versión 6.2.0

---
+++ Restaurante_Logica/Administracion_General_Restaurantes.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurante_Logica
+{
+    class Administracion_General_Restaurantes
+    {
+        Restaurante_Datos.RestauranteEntities conexionBD = new Restaurante_Datos.RestauranteEntities();
+
+        public bool insertarRestaurantes(String nombreRest, int categoria, int idMenu)
+        {
+            try
+            {
+
+                conexionBD.InsertarRestaurantes(nombreRest, categoria, idMenu);
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public bool modificarRestaurantes(int id, String nombreRest, int categoria, int idMenu)
+        {
+            try
+            {
+
+                conexionBD.ModificarRestaurante(id, nombreRest, categoria, idMenu);
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public bool eliminarRestaurantes(int id)
+        {
+            try
+            {
+
+                conexionBD.EliminarRestaurante(id);
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public static List<Restaurante_Datos.Restaurante> mostrarRestaurantes()
+        {
+            using (Restaurante_Datos.RestauranteEntities BD = new Restaurante_Datos.RestauranteEntities())
+            {
+
+                return BD.Restaurantes.ToList();
+            }
+        }
+    }
+}
