1b59da599956033aa59dffa847197405656ac9a0
johan Araya <arayax8025@outlook.com>
2019-12-04 23:26:58 -0600

    mostrar botones segun sesion

--- Restaurante_Logica/Inicio_Sesion.cs
+++ Restaurante_Logica/Inicio_Sesion.cs
@@ -40,7 +40,7 @@ namespace Restaurante_Logica
         }
 
   
-
+       
 
     }
 }
--- Restaurante_Menu/Site.Master.designer.cs
+++ Restaurante_Menu/Site.Master.designer.cs
@@ -13,6 +13,24 @@ namespace Restaurante_Menu {
     public partial class SiteMaster {
         
         /// <summary>
+        /// Control IniciarSesion.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlAnchor IniciarSesion;
+        
+        /// <summary>
+        /// Control Dashboard.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlAnchor Dashboard;
+        
+        /// <summary>
         /// Control MainContent.
         /// </summary>
         /// <remarks>
